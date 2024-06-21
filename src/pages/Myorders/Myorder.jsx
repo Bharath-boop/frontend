59947ce38f775b183f85e2d7b6cf697e2b8ca27c
Bharath kumar <bharathboopathi51550@gmail.com>
2024-06-21 13:06:43 +0530

    comp

--- src/pages/Myorders/Myorder.jsx
+++ src/pages/Myorders/Myorder.jsx
@@ -27,7 +27,7 @@ const Myorder = () => {
     <div className="my_orders">
       <h2>My Orders</h2>
       <div className="container">
-        {data.toReversed().map((e, i) => {
+        {data.map((e, i) => {
           return (
             <div key={i} className="my-order-order">
               <img src={assets.parcel_icon} alt="" />
